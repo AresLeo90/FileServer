ffc48dd6d02716af906ff99cdbfe4b63c9654ea3
Ares Leonardo <ares.leonardo@outlook.es>
2018-06-30 02:07:44 +0200

    Add Class AlumnoRepository and Interface IAlumnoRepository

---
+++ AlumnoRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Fileserver.Common.Model;
+using Fileserver.Infrastructure.Repository.Interfaces;
+
+namespace Fileserver.Infrastructure.Repository.Repositories
+{
+    public class AlumnoRepository
+    {
+
+    }
+
+}
